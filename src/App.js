48f9572e85af4f4bbfb654ccead9e84af6114b8d
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-11-30 00:09:31 +0600

    comment: default code cleand.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+/* import logo from './logo.svg';
+import './App.css'; */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      {/* <div className="App"> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <h1>clean code</h1>
     </div>
   );
 }

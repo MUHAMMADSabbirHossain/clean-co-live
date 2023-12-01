6fdbea6f5138a91fd9895b3ca249dc812af583e7
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-01 19:04:10 +0600

    Landing: banner upper section added.

--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import BucketGirl from "../../assets/image/bucketgirl.png";
 
 const Landing = () => {
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
-                    </div>
+        <div className="hero h-screen lg:h-[60vh] bg-accent mt-16">
+            <div className="hero-content flex-col lg:flex-row">
+
+                <div>
+                    <p>Best Quality</p>
+                    <h1 className="text-5xl font-bold">Professional Cleaning Service</h1>
+                    <p className="py-6 max-w-xl">At Clean Co, we go beyond cleaning - we perfect every detail. Our meticulous approach ensures a spotless space, prioritizing your satisfaction. Ready to experience the difference? Schedule your cleaning service now!</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+
+                <div className="h-[60vh] shrink-0">
+                    <img src={BucketGirl} className="h-full" />
                 </div>
             </div>
         </div>

2687beebc72cde60f634ee32eadbcf8d134f8f52
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-02 12:35:04 +0600

    navbar, sidebar: fixed for mobile resposive.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -64,8 +64,11 @@ const Navbar = ({ children }) => {
                 <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 min-h-full bg-base-200">
                     {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><NavLink to="/">HOME</NavLink></li>
+                    <li><NavLink to="/about">ABOUT</NavLink></li>
+                    <li><NavLink to="/services">SERVICES</NavLink></li>
+                    <li><NavLink to="/contact">CONTACT</NavLink></li>
+                    <li><NavLink to="/login">LOGIN</NavLink></li>
                 </ul>
             </div>
         </div>

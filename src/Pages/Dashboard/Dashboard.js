bbfc880895e026330a680461b89b05a7ffba7263
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-02 23:55:17 +0600

    dashboard.js, dashboardsidebar.js: sidebar created and linked with dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,27 +1,11 @@
 import React from 'react';
+import DashboardSidebar from '../../components/DashboardSidebar';
 
 const Dashboard = () => {
     return (
-        <div className="drawer lg:drawer-open flex mt-16 bg-accent">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
-
-            </div>
-
-            <div className="drawer-content flex flex-col items-center justify-center w-full">
-                {/* Page content here */}
-
-                asdf
-
-            </div>
-        </div>
+        <DashboardSidebar>
+            <h1>Clean Co.</h1>
+        </DashboardSidebar>
     );
 };
 

4a996213bea75f899a7e7bb34ebaa0f437ac8941
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-02 23:34:24 +0600

    dashboard: initaily deploy drawer, admin edited.

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>This is Dashboard</h1>
+        <div className="drawer lg:drawer-open flex mt-16 bg-accent">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+                    {/* Sidebar content here */}
+                    <li><a>Sidebar Item 1</a></li>
+                    <li><a>Sidebar Item 2</a></li>
+                </ul>
+
+            </div>
+
+            <div className="drawer-content flex flex-col items-center justify-center w-full">
+                {/* Page content here */}
+
+                asdf
+
+            </div>
         </div>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import useAdmin from '../hooks/useAdmin';
 
 const Navbar = ({ children }) => {
     const [dark, setDark] = useState(false);
     // console.log(dark);
+    const { pathname } = useLocation();
 
     const [admin] = useAdmin();
 
@@ -13,7 +14,14 @@ const Navbar = ({ children }) => {
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
                 {/* Navbar */}
-                <div className="w-full navbar bg-base-100 px-20">
+                <div className="w-full navbar bg-base-100 lg:px-20">
+
+                    {pathname.includes("dashboard") && (
+                        <label tabIndex={0} htmlFor="my-drawer-2" role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        </label>
+                    )}
+
                     <div className="flex-1 px-2 mx-2 text-2xl md:font-bold md:text-4xl">Clean Co.</div>
 
                     <div className="flex-none lg:hidden">

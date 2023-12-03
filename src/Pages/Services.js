12e99b7e9bcf52c9c3c4c2a44efdbc3749af8dec
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-03 19:37:49 +0600

    axios added in services.js

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        // axios.get("http://localhost:5000/service").then(res => setServices(res.data));
+
+        (async () => {
+            const res = await axios.get("http://localhost:5000/service");
+            setServices(res.data);
+        })()
+    }, []);
+    console.log("services", services);
+
     return (
         <div>
             <h1>This is Services</h1>
